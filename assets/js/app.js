3edeb84b234d9a3f0f0428a86f5809a2578eddb8
MarionFoto <marionacp@gmail.com>
2017-12-13 14:35:24 -0300

    fylf aplicación mobil con jquery

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	('.uno').hidde(); 
+        $(".cero").fadeOut(1500);
+    },3000);
+
+    setTimeout(function() {
+        $(".uno").fadeIn(1500);
+    },3000);
+
+})
